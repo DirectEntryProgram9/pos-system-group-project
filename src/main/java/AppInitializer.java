3ca2ed28fbcdc9efadcb855b465ea796a2e174e8
Pubudu Janith <pubudujanith123@gmail.com>
2022-08-07 15:43:08 +0530

    Disable stage options

--- src/main/java/AppInitializer.java
+++ src/main/java/AppInitializer.java
@@ -2,7 +2,9 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
@@ -17,8 +19,9 @@ AppInitializer extends Application {
     public void start(Stage primaryStage) throws IOException {
         Parent root = FXMLLoader.load(this.getClass().getResource("view/SplashScreenForm.fxml"));
         Scene scene = new Scene(root);
+        scene.setFill(Color.TRANSPARENT);
         primaryStage.setScene(scene);
-        primaryStage.setTitle("Splash Screen Form");
+        primaryStage.initStyle(StageStyle.TRANSPARENT);
         primaryStage.centerOnScreen();
         primaryStage.sizeToScene();
         primaryStage.show();
